abea5ce3faab72356919d6a4b6e5afa111f13f42
gunhong951 <gunhong951@gmail.com>
2022-10-27 22:26:46 +0900

    first commit

--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. you're at the polls index")
+    return HttpResponse("Hello, world. you're at the polls index. now 2022 10 27")
