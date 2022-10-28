4e24440c90876e1bb20dea323fc11f9bda8ffcca
gunhong951 <gunhong951@gmail.com>
2022-10-28 16:52:15 +0900

    forth commit

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -6,6 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
+#웹서버와 연결시키주는 파일
 
 import os
 
